91f7b806e555211b98085ba6e464e5ebb00ac2ef
Mark Davis <markymarkdavis@users.noreply.github.com>
2021-04-28 20:16:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     install_requires=[
         "dask[complete] == 2021.2.0",
         "dask-ml == 1.8.0",
-        "matplotlib > 2.2.0",
+        "matplotlib > 3.3.4",
         "pandas >= 1.1.3",
         "scikit-learn >= 0.23.2",
         "numpy >= 1.19.2",
