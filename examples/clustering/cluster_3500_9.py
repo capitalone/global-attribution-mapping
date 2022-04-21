8ccb432e9275d28dd4dea0321ba4975dee0513a6
hleerighter <helenleerighter@gmail.com>
2022-04-21 12:33:28 -0700

    add dask kmedoids run

--- examples/clustering/cluster_3500_9.py
+++ examples/clustering/cluster_3500_9.py
@@ -15,8 +15,11 @@ import time
 
 import numpy as np
 import pandas as pd
+import dask.array as da 
+import dask.dataframe as dd
 
 from gam.clustering import KMedoids
+from gam.dask_clustering import DaskKMedoids
 from gam.spearman_distance import spearman_squared_distance
 
 np.random.seed(42)
@@ -30,6 +33,7 @@ print(df.shape)
 kmed2 = KMedoids(
     5,
     dist_func=spearman_squared_distance,
+    batchsize=100,
     max_iter=10,
     tol=0.01,
     init_medoids='bandit',
@@ -37,6 +41,32 @@ kmed2 = KMedoids(
     verbose=True,
 )
 # attributions = np.array([(0.2, 0.8), (0.1, 0.9), (0.91, 0.09), (0.88, 0.12)])
+start_time = time.time()
+kmed2.fit(attributions, verbose=True)
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Finished test in {elapsed_time:.2f}")
+print(kmed2.centers)
+cluster_sizes = np.unique(kmed2.members, return_counts=True)[1]
+print(f'cluster sizes - {cluster_sizes}')
+# test that 2 attributions are in each cluster
+# assert(sum(kmedoids_2.members) == 2)
+
+
+
+""""Run kmedoids on sample attributions"""
+kmed2 = DaskKMedoids(
+    5,
+    dist_func=spearman_squared_distance,
+    batchsize=100,
+    max_iter=10,
+    tol=0.01,
+    init_medoids='bandit',
+    swap_medoids="bandit",
+    verbose=True,
+)
+# attributions = np.array([(0.2, 0.8), (0.1, 0.9), (0.91, 0.09), (0.88, 0.12)])
+attributions = dd.from_pandas(df, npartitions=4).to_dask_array(lengths=True)
 start_time = time.time()
 kmed2.fit(attributions, verbose=True)
 end_time = time.time()
