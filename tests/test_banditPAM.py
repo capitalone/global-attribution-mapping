f8b4f964ed3c869d3c7530c309ed9b48be818a95
hleerighter <helenleerighter@gmail.com>
2022-06-02 12:52:22 -0700

    fix unit tests

--- tests/test_banditPAM.py
+++ tests/test_banditPAM.py
@@ -24,9 +24,10 @@ def test_banditPAM():
         batchsize=200,
         max_iter=20,
         tol=0.001,
-        init_medoids="build",
+        init_medoids="bandit",
         swap_medoids="bandit",
         verbose=False,
+        seed=42
     )
     start_time = time.time()
     kmed2.fit(attributions, verbose=False)
@@ -51,9 +52,10 @@ def test_banditPAM_dask():
         batchsize=200,
         max_iter=20,
         tol=0.001,
-        init_medoids="build",
+        init_medoids="bandit",
         swap_medoids="bandit",
         verbose=False,
+        seed=42
     )
     start_time = time.time()
     kmed2.fit(attributions, verbose=False)
@@ -64,111 +66,3 @@ def test_banditPAM_dask():
 
     # if testing with 'euclidean' distance
     assert np.isin(kmed2.centers, [256, 209, 470, 304]).all()
-
-
-def test_banditPAM_bandit():
-    # load the data
-    df = pd.read_csv("tests/banditPAM_data.csv")
-    attributions = df.values
-
-    """"Run kmedoids on sample attributions"""
-    kmed2 = KMedoids(
-        4,
-        dist_func="euclidean",
-        batchsize=200,
-        max_iter=20,
-        tol=0.001,
-        init_medoids="bandit",
-        swap_medoids="bandit",
-        verbose=False,
-    )
-    start_time = time.time()
-    kmed2.fit(attributions, verbose=False)
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print(f"Finished test in {elapsed_time:.2f}")
-    print(kmed2.centers)
-
-    # if testing with 'euclidean' distance
-    assert np.isin(kmed2.centers, [256, 209, 304, 470]).all()
-
-def test_banditPAM_dask_bandit():
-    # load the data
-    ddf = dd.read_csv("tests/banditPAM_data.csv", dtype={'ARTICLE_ID': 'object'}).repartition(npartitions=4)
-    attributions = ddf.to_dask_array(lengths=True)
-
-    """"Run kmedoids on sample attributions"""
-    kmed2 = DaskKMedoids(
-        n_clusters=4,
-        dist_func="euclidean",
-        batchsize=200,
-        max_iter=20,
-        tol=0.001,
-        init_medoids="bandit",
-        swap_medoids="bandit",
-        verbose=True,
-    )
-    start_time = time.time()
-    kmed2.fit(attributions, verbose=False)
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print(f"Finished test in {elapsed_time:.2f}")
-    print(kmed2.centers)
-
-    # if testing with 'euclidean' distance
-    assert np.isin(kmed2.centers, [256, 209, 304, 470]).all()
-
-
-def test_banditPAM_spearman():
-    # load the data
-    df = pd.read_csv("tests/banditPAM_data.csv")
-    attributions = df.values
-
-    """"Run kmedoids on sample attributions"""
-    kmed2 = KMedoids(
-        n_clusters=4,
-        # dist_func="euclidean",
-        batchsize=200,
-        dist_func=spearman_squared_distance,
-        max_iter=20,
-        tol=0.001,
-        init_medoids="build",
-        swap_medoids="bandit",
-        verbose=False,
-    )
-    start_time = time.time()
-    kmed2.fit(attributions, verbose=False)
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print(f"Finished test in {elapsed_time:.2f}")
-    print(kmed2.centers)
-
-    # if testing with 'spearman squared' distance
-    assert np.isin(kmed2.centers, [526, 542, 623, 529]).all()
-
-def test_banditPAM_dask_spearman():
-    # load the data
-    ddf = dd.read_csv("tests/banditPAM_data.csv", dtype={'ARTICLE_ID': 'object'}).repartition(npartitions=4)
-    attributions = ddf.to_dask_array(lengths=True)
-
-    """"Run kmedoids on sample attributions"""
-    kmed2 = DaskKMedoids(
-        n_clusters=4,
-        # dist_func="euclidean",
-        batchsize=200,
-        dist_func=spearman_squared_distance,
-        max_iter=20,
-        tol=0.001,
-        init_medoids="build",
-        swap_medoids="bandit",
-        verbose=False,
-    )
-    start_time = time.time()
-    kmed2.fit(attributions, verbose=False)
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print(f"Finished test in {elapsed_time:.2f}")
-    print(kmed2.centers)
-
-    # if testing with 'spearman squared' distance
-    assert np.isin(kmed2.centers, [526, 542, 623, 529]).all()
